af2df200defc6b2b6aec03aa9a2a8f77bcb090b8
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2017-10-17 19:56:08 +1100

    finished the challenging part. still more to come tomorrow

--- code/Existfun.v
+++ code/Existfun.v
@@ -8,7 +8,7 @@ Require Import Bool.Sumbool.
 Require Import Bool.Bool.
 Require Import Coq.Logic.ConstructiveEpsilon.
 Require Import Permutation.
- (* Require Import Coq.ZArith.ZArith.
+(* Require Import Coq.ZArith.ZArith.
  Require Import ListLemma. *)
 Import ListNotations.
 Require Import
@@ -51,9 +51,9 @@ Qed.
 
 Lemma complementary_filter_In : forall
     (A : Type) (l : list A) (f : A -> bool) (g : A -> bool)
-    (H : forall x, In x l -> f x = negb (g x)),  
-  Permutation l (filter f l ++ filter g l).
-Proof with auto.                                           
+    (H : forall x, In x l -> f x = negb (g x)),
+    Permutation l (filter f l ++ filter g l).
+Proof with auto.
   intros A l f g H.
   induction l...
   simpl.
@@ -74,14 +74,14 @@ Qed.
 
 
 
-Section Cand.    
+Section Cand.
 
   Variable A : Type.
   Variable P : A -> A -> Prop.
   Hypothesis Adec : forall (c d : A), {c = d} + {c <> d}. (* A is decidable *)
   Hypothesis Pdec : forall c d, {P c d} + {~P c d}. (* P is decidable *)
-  
-  
+
+
   (* A is finite. finite : Type -> Type *)
   Definition finite := existsT (l : list A), forall (a : A), In a l.
 
@@ -89,37 +89,53 @@ Section Cand.
   Definition vl (l : list A) :=
     exists (f : A -> nat), forall (c d : A), In c l -> In d l -> (P c d <-> (f c < f d)%nat).
 
- 
-  Fixpoint listmax (A : Type) (l : list A) (f : A -> nat) : nat :=
+
+  Fixpoint listmax (f : A -> nat) (l : list A): nat :=
     match l with
     | [] => O
-    | h :: t => max (f h) (listmax A t f)
+    | [h] => f h
+    | h :: t => max (f h) (listmax f t)
     end.
- 
+
+  (*
+  Lemma listmax_upperbound :
+    forall (l : list A) (H : l <> nil)
+           (s : nat) (f : A -> nat),
+      listmax (map f l) < s <-> forall (x:A), In x l /\ f x < s.
+  Proof.
+  Admitted. *)
+
+
+
     
+  
   Lemma validity_after_remove_cand :
     forall (l : list A) (a0 : A),
       vl (a0 :: l) <->
       vl l /\ ~P a0 a0 /\
-      (forall (c d e : A), In c l -> In d l -> In e l ->  P c d -> P d e -> P c e) /\ 
+      (forall (c e : A), In c l -> In e l ->  P c a0 -> P a0 e -> P c e) /\
       ((exists (a0' : A), In a0' l /\ forall (x : A), In x l -> (P a0 x <-> P a0' x) /\
-                                                    (P x a0 <-> P x a0')) \/
+                                                                (P x a0 <-> P x a0')) \/
        (* forall (x : A), In x l -> P x a0 \/ P a0 x *)
        (forall (x : A), In x l -> (P x a0 /\ ~P a0 x)
                                   \/ (P a0 x /\  ~P x a0))
-       ).
+      ).
   Proof.
     unfold vl; split; intros.
-    destruct H as [f H]. 
+    destruct H as [f H].
     split.
     exists f. firstorder.
-    
+
     split. unfold not. intros. pose proof (proj1 (H a0 a0 (in_eq a0 l) (in_eq a0 l)) H0).
     omega.
 
-    split.  intros.  
+    split. intros.
+    pose proof (H c a0 (in_cons _ c l H0) (in_eq a0 l)).
+    pose proof (H a0 e (in_eq a0 l) (in_cons _ e l H1)).
+    firstorder.
+
     assert (Hnat : forall x y : nat, {x = y} + {x <> y}) by (auto with arith).
-      
+
     pose proof (in_dec Hnat (f a0) (map f l)).  clear Hnat.
     destruct H0.
     apply in_map_iff in i. destruct i as [a [Hl Hr]].
@@ -145,7 +161,7 @@ Section Cand.
     right.
     intros x Hx.
 
-    destruct (lt_eq_lt_dec (f a0) (f x)) as [[H1 | H1] | H1]. 
+    destruct (lt_eq_lt_dec (f a0) (f x)) as [[H1 | H1] | H1].
     pose proof (H a0 x (in_eq a0 l) (or_intror Hx)).
     firstorder.
 
@@ -166,11 +182,11 @@ Section Cand.
 
     (* finally finished the first half. feeling great :) *)
 
-    destruct H as [[f H1] [Ht [[a [H2 H3]] | H2]]].    
+    destruct H as [[f H1] [Ht [Ht1 [[a [H2 H3]] | H2]]]].
     (* From H3, I know that f a = f a0  so I am going to supply same function *)
 
     exists (fun c => if Adec c a0 then f a else f c). intros c d H4 H5. destruct H4, H5.
-    split; intros. 
+    split; intros.
     rewrite <- H in H4. rewrite <- H0 in H4.
     firstorder. rewrite <- H0 in H4.
     rewrite -> H in H4. omega.
@@ -208,62 +224,62 @@ Section Cand.
     destruct (Adec d a0).
     subst. firstorder.
     subst. firstorder.
-      
-    (*
+
+    
     remember (filter (fun y => if Pdec y a0 then true else false) l) as l1.
     remember (filter (fun y => if Pdec a0 y then true else false) l) as l2.
-    assert (Ht1 : forall x, In x l1 -> P x a0 /\ ~P a0 x).
+    assert (Ht2 : forall x, In x l1 -> P x a0 /\ ~P a0 x).
     intros. rewrite Heql1 in H.
     pose proof (proj1 (filter_In _ _ _) H).
     destruct H0. pose proof (H2 x H0). destruct H4. auto.
     destruct (Pdec x a0).  firstorder.  inversion H3.
-    assert (Ht2 : forall x, In x l2 -> P a0 x /\ ~P x a0).
+    assert (Ht3 : forall x, In x l2 -> P a0 x /\ ~P x a0).
     intros. rewrite Heql2 in H.
     pose proof (proj1 (filter_In _ _ _) H).
     destruct H0. pose proof (H2 x H0). destruct H4.
     destruct (Pdec a0 x). firstorder. inversion H3. auto.
     remember (fun y : A => if Pdec y a0 then true else false) as f1.
-    remember (fun y : A => if Pdec a0 y then true else false) as g1. 
-    assert (Ht3 : forall x, In x l -> f1 x = negb (g1 x)).
-    intros. 
+    remember (fun y : A => if Pdec a0 y then true else false) as g1.
+    assert (Ht4 : forall x, In x l -> f1 x = negb (g1 x)).
+    intros.
     rewrite Heqf1.
     rewrite Heqg1.
-    destruct (Pdec x a0) eqn: Ht3.
-    destruct (Pdec a0 x) eqn: Ht4.
+    destruct (Pdec x a0) eqn: Ht4.
+    destruct (Pdec a0 x) eqn: Ht5.
     pose proof (H2 x H). destruct H0.
     firstorder. firstorder.
     auto.
-    destruct (Pdec a0 x) eqn: Ht4. auto.
+    destruct (Pdec a0 x) eqn: Ht6. auto.
     pose proof (H2 x H). destruct H0.
     firstorder. firstorder.
-    pose proof (complementary_filter_In _ l f1 g1 Ht3).
-    rewrite <- Heql1 in H. rewrite <- Heql2 in H. *)
-       
-    (* for a0,  take maximum of all the candidates which is preferred over 
-       a0 and add one to it. 
+    pose proof (complementary_filter_In _ l f1 g1 Ht4).
+    rewrite <- Heql1 in H. rewrite <- Heql2 in H. 
+
+    (* for a0,  take maximum of all the candidates which is preferred over
+       a0 and add one to it.
        a1, a2 ......, a0, ....., an
-       We don't need to change the values for candidates preferred over a0, but 
+       We don't need to change the values for candidates preferred over a0, but
        those who are less preferred over a0 should be shifted by 1 *)
 
     
     exists (fun x =>
-         match Adec x a0 with
-         | left _ =>
-           plus (S O)
-                (listmax A (filter (fun y => proj1_sig (bool_of_sumbool (Pdec y a0))) l) f)
-         | right _ => 
-           if andb (proj1_sig (bool_of_sumbool (Pdec a0 x)))
-                   (proj1_sig (bool_of_sumbool (in_dec Adec x l)))
-           then plus (S O) (f x)
-           else  (f x)
-         end).
+              match Adec x a0 with
+              | left _ =>
+                plus (S O)
+                     (listmax f (filter (fun y => if Pdec y a0 then true else false) l))
+              | right _ =>
+                if andb (if Pdec a0 x then true else false)
+                        (if (in_dec Adec x l) then true else false)
+                then plus (S (S O)) (f x)
+                else  (f x)
+              end).
 
     (* this code is for permuation of list *)
-    (*
+   
     split; intros.
     destruct H0, H3.
     congruence.
-
+ 
     rewrite <- H0. rewrite <- H0 in H4.
     destruct (Adec a0 a0).
     destruct (Adec d a0).
@@ -273,216 +289,170 @@ Section Cand.
     simpl. apply lt_n_S.
 
     (* remove unnecessary assumption *)
-    clear e. clear p. clear i.
+    clear e. clear p. clear i. clear n.
     pose proof Permutation_in.
     pose proof (H5 A l (l1 ++ l2) d H H3).
-    clear H5.
     apply in_app_iff in H6. destruct H6.
-    pose proof (Ht1 d H5). firstorder.
-    *)
-    
+    pose proof (Ht2 d H6). firstorder.
+    rewrite <- Heqf1.
+    rewrite <- Heql1.
 
-    
-    
-    split; intros.
-    destruct H, H0.
+    assert (Ht5: forall x, In x l1 -> forall y, In y l2 -> f x < f y).
+    intros. apply H1.
+    apply Permutation_sym in H.
+    pose proof (H5 A (l1 ++ l2) l x H). apply H9.
+    firstorder.
+    apply Permutation_sym in H.
+    pose proof (H5 A (l1 ++ l2) l y H). apply H9.
+    firstorder.
+    apply Ht1.
+    apply Permutation_sym in H.
+    pose proof (H5 A (l1 ++ l2) l x H). apply H9.
+    firstorder.
+    apply Permutation_sym in H.
+    pose proof (H5 A (l1 ++ l2) l y H). apply H9.
+    firstorder. firstorder. firstorder.
 
-    (* first case c = a0, d = a0 *)
-    congruence.
+    clear H. clear H5. clear Heql1. clear Ht2.
+    induction l1. simpl. omega.
+    simpl. destruct l1.
+    pose proof (Ht5 a (in_eq a []) d H6). omega.
+    apply Nat.max_lub_lt_iff. split.
+    pose proof (Ht5 a (in_eq a (a1 :: l1)) d H6).
+    omega. apply IHl1. firstorder.
 
-    (* second case c = a0, In d l *)
-    rewrite <- H. rewrite <- H in H3.
-    destruct (Adec a0 a0).
-    destruct (Adec d a0).
-    congruence.
-    destruct (Pdec a0 d).
-    destruct (in_dec Adec d l).
-    simpl. apply lt_n_S.
-    (* remove unneccesary lemma *)
-    clear H0. clear e. clear H3. clear n.
-    
-    induction l.
-    simpl. inversion i.
-    simpl. destruct (Pdec a a0).
-    simpl. destruct i. 
-    apply Nat.max_lub_lt_iff.
-    split. destruct H0.
-    pose proof (H2 a (in_eq a l)).
-    subst. firstorder.
- 
-    pose proof (H2 a (in_eq a l)).
-    subst. firstorder.
-    subst. apply Nat.max_lub_lt_iff.
-    split. apply H1. firstorder.  right. firstorder.
-    (* Can we say that P a c -> P c d -> P a d ? *)
-    admit.
-    apply IHl. firstorder.
-    intros. apply H2. right. auto. auto.
-    simpl. destruct i. 
-    
-    
-    
-    admit. 
-    congruence.
-    congruence.
-    congruence.
+    congruence.  congruence. congruence.
 
-    (* In c l and a0 = d *)
-    destruct (Adec c a0).
-    congruence.
-    destruct (Pdec a0 c).
-    destruct (in_dec Adec c l).
-    destruct (Adec d a0).
-    simpl. apply lt_n_S.
-    admit.
-    congruence.
-    congruence.
-    destruct (Adec d a0).
-    simpl. rewrite <- e in n0.
-    admit.
-    congruence.
-    destruct (Adec c a0).
-    destruct (Adec d a0).
-    congruence.
-    destruct (Pdec a0 d).
-    destruct (in_dec Adec d l).
-    simpl. apply lt_n_S.
-    admit.
-    congruence.
-    congruence.
-    destruct (Pdec a0 c).
-    destruct (in_dec Adec c l).
-    destruct (Adec d a0).
-    simpl. apply lt_n_S. admit.
-    simpl.
-    
-    
-  (* This proof is mostly followed by validity_after_remove_cand. *)
-  Lemma vl_or_notvl : forall l : list A, vl l + ~vl l.
-  Proof.
-    
-    induction l.
-    left. unfold vl. eexists.
-    intros c d Hc Hd; inversion Hc.
 
-    (* l := a :: l *)
-    pose proof (validity_after_remove_cand l a).
-    destruct IHl.
-    (* if P a a or ~ P a a *)
-    pose proof (Pdec a a).
-    (* I can not destruct H0 of type Prop because goal is of type Set. 
+
+
+
+
+    (* This proof is mostly followed by validity_after_remove_cand. *)
+    Lemma vl_or_notvl : forall l : list A, vl l + ~vl l.
+    Proof.
+
+      induction l.
+      left. unfold vl. eexists.
+      intros c d Hc Hd; inversion Hc.
+
+      (* l := a :: l *)
+      pose proof (validity_after_remove_cand l a).
+      destruct IHl.
+      (* if P a a or ~ P a a *)
+      pose proof (Pdec a a).
+      (* I can not destruct H0 of type Prop because goal is of type Set.
        We need to probably change the Pdec to forall c d, {P c d} + {~ P c d} ? *)
-    (* After this proof is very easy. 
-       If P a a then we can not construct the valid (a :: l) from vl and go right. 
-       If ~P a a then we can construct the the valid (a :: l) from vl. 
-       In (f a) (map f l) + ~ In (f a) (map f l). 
-       If In (f a) (map f l) then we there is some element, a0, in l such that 
-       f a = f a0 and we can  discharge existential 
+      (* After this proof is very easy.
+       If P a a then we can not construct the valid (a :: l) from vl and go right.
+       If ~P a a then we can construct the the valid (a :: l) from vl.
+       In (f a) (map f l) + ~ In (f a) (map f l).
+       If In (f a) (map f l) then we there is some element, a0, in l such that
+       f a = f a0 and we can  discharge existential
        If ~In (f a) (map f l) then we can split the l into two sorted list, l1 , l2
        and discharge forall x, In x l -> P a x \/ P x a *)
-    
-    
-    (* if ~vl l then adding candidate would also not make it valid 
+
+
+      (* if ~vl l then adding candidate would also not make it valid
        got for right *)
-    admit.
-    right. unfold not. intros.
-    destruct n. firstorder.
+      admit.
+      right. unfold not. intros.
+      destruct n. firstorder.
     Admitted.
-    
 
-    
-  Definition valid := exists (f : A -> nat), forall (c d : A), P c d <-> (f c < f d)%nat.
 
-  Lemma from_vl_to_valid : forall (l : list A), ((forall a : A, In a l) -> valid <-> vl l).
-  Proof. 
-    intros l Ha. split; intros.
-    unfold valid in H.
-    unfold vl.
-    destruct H as [f H].
-    firstorder.
-    unfold vl in H. unfold valid.
-    destruct H as [f H].
-    exists f. split; intros.
-    apply H; auto.
-    apply H; auto.
-  Qed.
-  
-    
-  Lemma decidable_valid : finite -> {valid} + {~valid}.
-  Proof.
-    unfold finite, valid.
-    intros H. destruct H as [l Hin].
 
-    
+    Definition valid := exists (f : A -> nat), forall (c d : A), P c d <-> (f c < f d)%nat.
 
-    
+    Lemma from_vl_to_valid : forall (l : list A), ((forall a : A, In a l) -> valid <-> vl l).
+    Proof.
+      intros l Ha. split; intros.
+      unfold valid in H.
+      unfold vl.
+      destruct H as [f H].
+      firstorder.
+      unfold vl in H. unfold valid.
+      destruct H as [f H].
+      exists f. split; intros.
+      apply H; auto.
+      apply H; auto.
+    Qed.
+
+
+    Lemma decidable_valid : finite -> {valid} + {~valid}.
+    Proof.
+      unfold finite, valid.
+      intros H. destruct H as [l Hin].
+
+
+
+
 End Cand.
 
 Check decidable_valid.
 
 
 
-  
-    
-  Lemma validity_after_remove_cand :
-    forall (P : A -> A -> Prop) (l : list A) (Hpdec : forall c d, P c d \/ ~P c d),
-      valid P l <->
-      exists (c : A), forall (d : A), In c l /\ In d l /\  (equal_rank P c d \/ (P c d \/ c = d \/ P d c))
-                            /\ valid P (remove A_dec d l).
-  Proof.
-    unfold valid, equal_rank.
-    split; intros.
-    destruct H as [f H].
 
-    (* induction on l *)
-    induction l.
-    (* admit the empty case for the moment *)
-    admit.
-    (* a :: l and assume c = a *)
-    exists a. intros d.
-    (* Either d is a or d is inside the list *)
-    destruct (A_dec a d).
-    split. apply in_eq.
-    split. firstorder.
-    split. (* At this point we have a = d in assumption and 
+
+Lemma validity_after_remove_cand :
+  forall (P : A -> A -> Prop) (l : list A) (Hpdec : forall c d, P c d \/ ~P c d),
+    valid P l <->
+    exists (c : A), forall (d : A), In c l /\ In d l /\  (equal_rank P c d \/ (P c d \/ c = d \/ P d c))
+                                    /\ valid P (remove A_dec d l).
+Proof.
+  unfold valid, equal_rank.
+  split; intros.
+  destruct H as [f H].
+
+  (* induction on l *)
+  induction l.
+  (* admit the empty case for the moment *)
+  admit.
+  (* a :: l and assume c = a *)
+  exists a. intros d.
+  (* Either d is a or d is inside the list *)
+  destruct (A_dec a d).
+  split. apply in_eq.
+  split. firstorder.
+  split. (* At this point we have a = d in assumption and
       ~ P a d /\ ~ P d a \/ P a d \/ a = d \/ P d a in goal.
-    I think a = d should mean that either they are equal_rank or a = d and 
+    I think a = d should mean that either they are equal_rank or a = d and
     this should be used to discharge the goal *)
-    right. firstorder.
-    exists f.  split; intros. rewrite e in Hc, Hd.
-    pose proof (H c d0). simpl in *. firstorder.
-    rewrite e in Hc, Hd.
-    pose proof (H c d0). simpl in *. firstorder.
-    
-    (* a <> d *)
-    split. apply in_eq.
-    split. pose proof (H a d (in_eq a l)). firstorder.
-    split.  admit.
-    (* At this point we a <> d in assumption and  
-       ~ P a d /\ ~ P d a \/ P a d \/ a = d \/ P d a in goal.
-      I think a <> d should mean that either P a d or P d a and this 
-      should discharge the assumption
-  *)
-    exists f. split; intros. simpl in Hc, Hd. destruct (A_dec d a).
-    symmetry in e. pose proof (n e). inversion H1.
-    simpl in *. destruct Hc, Hd.  firstorder.
-    pose proof (H c d0). firstorder.
-    pose proof (H c d0). firstorder.
-    pose proof (H c d0). firstorder.
-    pose proof (H c d0). firstorder.
-    
-    (* reverse direction *)
-    destruct H as [x H]. Check fold_left.
-    pose proof (H x). destruct H0 as [H1 [H2 [H3 [f H4]]]].
-    
-    induction l. firstorder.
+  right. firstorder.
+  exists f.  split; intros. rewrite e in Hc, Hd.
+  pose proof (H c d0). simpl in *. firstorder.
+  rewrite e in Hc, Hd.
+  pose proof (H c d0). simpl in *. firstorder.
 
-    (* either a = c or a <> c *)
-    
-    
+  (* a <> d *)
+  split. apply in_eq.
+  split. pose proof (H a d (in_eq a l)). firstorder.
+  split.  admit.
+  (* At this point we a <> d in assumption and
+       ~ P a d /\ ~ P d a \/ P a d \/ a = d \/ P d a in goal.
+      I think a <> d should mean that either P a d or P d a and this
+      should discharge the assumption
+   *)
+  exists f. split; intros. simpl in Hc, Hd. destruct (A_dec d a).
+  symmetry in e. pose proof (n e). inversion H1.
+  simpl in *. destruct Hc, Hd.  firstorder.
+  pose proof (H c d0). firstorder.
+  pose proof (H c d0). firstorder.
+  pose proof (H c d0). firstorder.
+  pose proof (H c d0). firstorder.
+
+  (* reverse direction *)
+  destruct H as [x H]. Check fold_left.
+  pose proof (H x). destruct H0 as [H1 [H2 [H3 [f H4]]]].
+
+  induction l. firstorder.
+
+  (* either a = c or a <> c *)
+
+
   Lemma dec_now : forall (P : A -> A -> Prop),
       (forall c d, P c d \/ ~P c d) ->
       {valid P} + {~valid P}.
   Proof.
     intros P H. unfold valid.
-    
