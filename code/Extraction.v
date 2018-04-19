327e3b285c15c51258e3400a5c6c53e8ede73470
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2018-04-19 15:13:46 +1000

    changed nat and Z to Int

--- code/Extraction.v
+++ code/Extraction.v
@@ -4,7 +4,7 @@ Require Import EncryptionSchulze.
 Require Import Coq.Strings.String.
 Require Import ZArith.
 Require Import EqNat.
-
+Require Import Coq.extraction.ExtrOcamlZInt.
 (*
 Extraction Language Haskell.
 Extraction "lib.hs" schulze_winners_pf. *)
