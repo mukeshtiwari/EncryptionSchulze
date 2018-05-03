1a8ff5eb6cd641e7c58be2fea187addecfa3fc1b
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2018-05-03 19:02:24 +1000

    added bindings from java code, and everything is working

--- code/javaocamlbinding.ml
+++ code/javaocamlbinding.ml
@@ -11,16 +11,23 @@ object
 end
 
 
+
 class%java crypto_java "schulze.HelloWorld" = 
 object
         method [@static] generate_privatekey : big_integer = "generateSK"
         method [@static] generate_publickey : big_integer -> big_integer = "generatePK"
         method [@static] enc_ballot_wrapper : big_integer array -> big_integer -> big_integer array = "encBallotWrapper"
+        method [@static] enc_ballot_mult_wrapper : big_integer array -> big_integer array -> big_integer array = "encBallotMultWrapper"
+        method [@static] dec_ballot_wrapper : big_integer array -> big_integer -> big_integer array = "decBallot"
+        method [@static] row_permutation_wrapper : big_integer array -> big_integer -> big_integer array = "rowShuffle"
+        method [@static] column_permutation_wrapper : big_integer array -> big_integer -> big_integer array = "columnShuffle"
 end
 
 
+(* this function creates java array from list of strings*)
 let create_array_from_list lst =
     let len = List.length lst in
+    let nlst = List.map Big_integer.create lst in
     let cls = Jclass.find_class "java/math/BigInteger" in
     let ballot = Jarray.create_object cls Java.null len in
     let rec loop i = function
@@ -29,16 +36,84 @@ let create_array_from_list lst =
             loop (i + 1) tl
         | []        -> ()
     in
-    loop 0 lst;
+    loop 0 nlst;
     ballot
 
+(* this function creates ocaml list of strings from java array *)
+let create_list_from_array arr = 
+    let len = Jarray.length arr in 
+    let rec loop i acc = 
+     if i >= len then (List.rev acc)
+     else let v = Big_integer.to_string (Jarray.get_object arr i) in
+           loop (i + 1) (v :: acc) in
+    loop 0 []
+
+
+(* This function takes list of string from lib.ml code and it is basically a plaintext ballot which is encrypte here and returned as list of string *)
+let enc_ballot lst publickey = 
+  let arr = create_array_from_list lst in
+  let encarry = Crypto_java.enc_ballot_wrapper arr publickey in 
+  create_list_from_array encarry
+
+(* this function takes two list of strings (which are basically big integers in string) and returns the sum *)
+let homomorphic_addition lst1 lst2 = 
+  let arr1 = create_array_from_list lst1 in
+  let arr2 = create_array_from_list lst2 in
+  let arr3 = Crypto_java.enc_ballot_mult_wrapper arr1 arr2 in
+  create_list_from_array arr3
+
+(* This function decrypts encrypted ballot *)
+let dec_ballot lst privatekey = 
+  let arr = create_array_from_list lst in 
+  let decarr = Crypto_java.dec_ballot_wrapper arr privatekey in
+  create_list_from_array decarr
+
+(* This function calculates row permuted array *)  
+let row_perm lst publickey = 
+  let arr = create_array_from_list lst in
+  let rowpermarray = Crypto_java.row_permutation_wrapper arr publickey in 
+  create_list_from_array rowpermarray
+
+(* this function calculates column permuted array *)
+let column_perm lst publickey = 
+  let arr = create_array_from_list lst in 
+  let colpermarray = Crypto_java.column_permutation_wrapper arr publickey in 
+  create_list_from_array colpermarray
+  
+(*                 
+let _ = 
+   let lst = List.map string_of_int [0; 0; 0; 0; 0; 0; 0; 0; 0] in 
+   let b = create_list_from_array (create_array_from_list lst) in 
+   List.iter (fun x -> print_endline x) b *)
+
 (* We are assuming only 3 candidates for the moment *)    
+(*
 let starting_margin = List.map string_of_int [0; 0; 0; 0; 0; 0; 0; 0; 0]
 let plaintext = List.map (List.map string_of_int) [[0; 1; 1; 0; 0; 1; 0; 0; 0]; [0; 1; 1; 0; 0; 1; 0; 0; 0]; [0; 1; 1; 0; 0; 0; 0; 1; 0]; [0; 1; 1; 0; 0; 1; 0; 0; 0]; [0; 0; 0; 1; 0; 1; 1; 0; 0]; 
                  [0; 0; 0; 1; 0; 0; 1; 1; 0]; [0; 1; 0; 0; 0; 0; 1; 1; 0]; [0; 1; 1; 0; 0; 0; 0; 1; 0]; [0; 0; 1; 1; 0; 1; 0; 0; 0]; [0; 1; 1; 0; 0; 1; 0; 0; 0]]
+*)
+let invalid_ballot = (List.map string_of_int [1;1;1;1;1;1;1;1;1])
 
 
-let encrypt_margin pkey bal = Crypto_java.enc_ballot_wrapper bal pkey
+
+
+
+
+let _ = 
+ let pkey = Big_integer.create "49228593607874990954666071614777776087" in 
+ let skey = Big_integer.create "60245260967214266009141128892124363925" in
+ let encinvbal = enc_ballot invalid_ballot pkey in
+ let rperm = row_perm encinvbal pkey in 
+ let cperm = column_perm rperm pkey in 
+ let decinvbal = dec_ballot cperm skey in
+ List.iter (fun x -> print_endline x) encinvbal;
+ List.iter (fun x -> print_endline x) rperm;
+ List.iter (fun x -> print_endline x) cperm;
+ List.iter (fun x -> print_endline x) decinvbal
+
+
+
+
 (*    
 let () = 
    let strl = ["1"; "1"; "1"; "1"; "1"; "1"; "1"; "1"] in  
@@ -57,15 +132,27 @@ let () =
 
 (* Don't expose any internal class so try to operate in BigInteger world because it will make the code and life easier *)
 
-
+(*
 let _ =  
    let prikey = Crypto_java.generate_privatekey () in
    let pubkey = Crypto_java.generate_publickey prikey in 
    let zero_marg = encrypt_margin pubkey (create_array_from_list (List.map Big_integer.create starting_margin)) in
+   let data_ballot = List.map (fun x -> create_array_from_list (List.map Big_integer.create x)) plaintext in
+   let enc_ballot = List.map (fun x -> encrypt_margin pubkey x) data_ballot in 
+   print_endline ("private key = " ^ (Big_integer.to_string prikey));
+   print_endline ("public key = " ^ (Big_integer.to_string pubkey));
    for i = 0 to 17 do
         print_endline (Big_integer.to_string (Jarray.get_object zero_marg i))
-   done
-(* with Java.exception e -> 
-   Jthrowable.print_stack_trace e;
-   failwith "" *)
+   done;
+   print_endline ("finished zero margin encryption");
+   for i = 0 to 9 do
+        print_endline "starting to encrypt ballot";
+        for j = 0 to 17 do
+                 print_endline (Big_integer.to_string (Jarray.get_object (List.nth enc_ballot i) j))
+        done;
+        print_endline "finished encryption";
+   done;
+*)
+
+
 
