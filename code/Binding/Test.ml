161f6c00a97d0e9ea5da0d319321a0982bb6fdfa
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2018-12-04 17:39:13 +1100

    Added Encryption and Decryption function.

--- code/Binding/Test.ml
+++ code/Binding/Test.ml
@@ -4,6 +4,7 @@ let () = Java.init [| "-Djava.class.path=ocaml-java/bin/ocaml-java.jar:javacrypt
 class%java big_integer "java.math.BigInteger" =
 object 
         initializer(create : string -> _)
+        method to_string : string = "toString"
 end
 
 (* binding to prime *)
@@ -25,10 +26,21 @@ object
 end
 
 (* Element *)
-
 class%java element "ch.bfh.unicrypt.math.algebra.general.interfaces.Element" = 
-object 
+object
         method to_string : string = "toString"
+end
+
+(* Get Element from BigInteger https://github.com/bfh-evg/unicrypt/blob/master/src/main/java/ch/bfh/unicrypt/math/algebra/general/interfaces/Set.java *)
+class%java set "ch.bfh.unicrypt.math.algebra.general.interfaces.Set" = 
+object 
+      method to_string : string = "toString"
+end
+
+
+class%java tuple "ch.bfh.unicrypt.math.algebra.general.classes.Tuple" = 
+object 
+     method to_string : string = "toString"
 end
 
 class%java zmod "ch.bfh.unicrypt.math.algebra.dualistic.classes.ZMod" = object end 
@@ -41,7 +53,8 @@ object
 end
 
 class%java zmod_element "ch.bfh.unicrypt.math.algebra.dualistic.classes.ZModElement" = 
-object 
+object
+       inherit element 
        initializer(get_zmod_element : zmod -> big_integer -> _)
        method to_string : string = "toString"
 end
@@ -72,7 +85,7 @@ end
 class%java gstar_mod_element "ch.bfh.unicrypt.math.algebra.multiplicative.classes.GStarModElement" =
 object
         inherit element
-        initializer(get_element : gstar_mod -> big_integer -> _) 
+        initializer(get_element : gstar_mod -> big_integer -> _)
         method to_string : string = "toString"
 end
 
@@ -81,6 +94,8 @@ end
 class%java elgamal_encryption_scheme "ch.bfh.unicrypt.crypto.schemes.encryption.classes.ElGamalEncryptionScheme" = 
 object
         method [@static] get_scheme : element -> elgamal_encryption_scheme = "getInstance"
+        method encrypt_element : element -> element -> element = "encrypt"
+        method decrypt_element : element -> element -> element = "decrypt"
         method to_string : string = "toString"
 end 
 
@@ -98,7 +113,7 @@ let safe_prime (p : 'a Big_integer.t') =
 let group_from_safe_prime (p : 'a Safe_prime.t') = 
     Gstar_mod_safe_prime.get_instance p
 
-(* Construct generator data structure *)
+(* Construct generator data structure. Use this function to construct GStarModElement or Element *)
 let generator_from_group grp gen = 
     Gstar_mod_element.get_element grp gen 
 
@@ -109,27 +124,46 @@ let elgamal_encryption_scheme_from_generator gen =
 (* This function is exist because we have already generated prime, grp, privatekey, and publickey using some 
    external tool. Now we need to convert them into Java data structure passed through OCaml. This function takes 
    publickey as big_integer and returns Java object *)
-let generate_public_key grp publickey =  
-    generator_from_group grp publickey
+let generate_public_key grp publickey = 
+    Gstar_mod_element.get_element grp publickey 
+    
+(* Use this function to generate elements of a group *)
+let generate_element_of_group grp element = 
+     Gstar_mod_element.get_element grp element
 
 (* this function take grp and privatekey and returns java data strucutre for privatekey *)
 let get_zmod_prime grp privatekey = 
     let zmodp = Gstar_mod_prime.get_zmod_order grp in
     Zmod_element.get_zmod_element zmodp privatekey
+
+(* encryption function which takes grp, generator, publickey and msg, and returns 
+   encrypted message as Pair of element *)
+let encrypt_message grp gen publickey (msg : 'a Element.t') = 
+    let elgamal = elgamal_encryption_scheme_from_generator gen in 
+    Elgamal_encryption_scheme.encrypt_element elgamal publickey msg  
+
+
+let decrypt_message grp gen publickey privatekey encmsg = 
+   let elgamal = elgamal_encryption_scheme_from_generator gen in 
+   Elgamal_encryption_scheme.decrypt_element elgamal privatekey encmsg  
  
 let () = 
    let safep = safe_prime (big_int_from_string  "170141183460469231731687303715884114527") in
-   let gp = group_from_safe_prime safep in
-   let gen = generator_from_group gp (big_int_from_string "4") in 
+   let grp = group_from_safe_prime safep in
+   let gen = generator_from_group grp (big_int_from_string "4") in 
    let elgamal = elgamal_encryption_scheme_from_generator gen in
-   let publickey = generate_public_key gp (big_int_from_string "49228593607874990954666071614777776087") in
-   let zmodprime = get_zmod_prime gp (big_int_from_string "60245260967214266009141128892124363925") in 
+   let publickey = generate_public_key grp (big_int_from_string "49228593607874990954666071614777776087") in
+   let privatekey = get_zmod_prime grp (big_int_from_string "60245260967214266009141128892124363925") in
+   let encm = encrypt_message grp gen publickey (generate_element_of_group grp (big_int_from_string  "5444")) in
+   let decm = decrypt_message grp gen publickey privatekey encm in
    print_endline (Prime.to_string safep);
-   print_endline (Gstar_mod_safe_prime.to_string gp);
+   print_endline (Gstar_mod_safe_prime.to_string grp);
    print_endline (Gstar_mod_element.to_string gen);
    print_endline (Elgamal_encryption_scheme.to_string elgamal);
    print_endline (Gstar_mod_element.to_string publickey);
-   print_endline (Zmod_element.to_string zmodprime)
+   print_endline (Zmod_element.to_string privatekey);
+   print_endline (Element.to_string encm);
+   print_endline (Element.to_string decm)
 
 
 
