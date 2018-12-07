a0723692b5a75cfdb7b89a538877d29fc66af640
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2018-12-07 16:03:55 +1100

    finished almost all the functions except decryption zero knowledge proof and changing ballots to function

--- code/Binding/Test.ml
+++ code/Binding/Test.ml
@@ -25,8 +25,9 @@ object
         method to_string : string = "toString"
 end
 
+(* cyclic group *)
+class%java cyclic_group  "ch.bfh.unicrypt.math.algebra.general.interfaces.CyclicGroup" = object  end
 (* Element *)
-
 class%java element "ch.bfh.unicrypt.math.algebra.general.interfaces.Element" = 
 object
         method to_string : string = "toString"
@@ -37,18 +38,25 @@ class%java set "ch.bfh.unicrypt.math.algebra.general.interfaces.Set" =
 object
 
        method get_random_element : element = "getRandomElement"
+       method to_string : string = "toString"
 end
 
 
-(*
+
 class%java tuple "ch.bfh.unicrypt.math.algebra.general.classes.Tuple" = 
 object
-     
+     inherit element   
      method [@static] get_instance : element -> int -> tuple = "getInstance"
      method get_first : element = "getFirst"    
      method get_last : element = "getLast" 
      method get_at : int -> element = "getAt"
      method to_string : string = "toString"
+end
+
+class%java triple "ch.bfh.unicrypt.math.algebra.general.classes.Triple" = 
+object 
+    inherit tuple
+    method [@static] get_instance : element -> element -> element -> triple = "getInstance"
 end
 
 class%java pair "ch.bfh.unicrypt.math.algebra.general.classes.Pair" = 
@@ -57,7 +65,7 @@ object
      method [@static] construct_pair : element -> element -> pair = "getInstance"
      method get_second : element = "getSecond"
      method to_string : string = "toString"
-end *)
+end
 
 class%java zmod "ch.bfh.unicrypt.math.algebra.dualistic.classes.ZMod" = object end 
 
@@ -76,7 +84,8 @@ object
 end
 
 class%java gstar_mod "ch.bfh.unicrypt.math.algebra.multiplicative.classes.GStarMod" = 
-object 
+object  
+        inherit cyclic_group
         method to_string : string = "toString"
 end
 
@@ -162,6 +171,8 @@ class%java mixer "ch.bfh.unicrypt.crypto.mixer.interfaces.Mixer" =
 object
        
        method get_permutation_group : permutation_group = "getPermutationGroup"
+       method generate_randomizations : tuple = "generateRandomizations"
+       method shuffle : tuple -> permutation_element -> tuple -> tuple = "shuffle"
        method to_string : string = "toString"
 end
 
@@ -173,10 +184,40 @@ object
         method to_string : string = "toString"
 end
 
+class%java randomization_commitment_scheme "ch.bfh.unicrypt.crypto.schemes.commitment.interfaces.RandomizedCommitmentScheme" =
+object 
+        method get_randomization_space : set = "getRandomizationSpace"
+        method commit : element -> element -> element = "commit"
+end
+
 class%java permutation_commitment_scheme "ch.bfh.unicrypt.crypto.schemes.commitment.classes.PermutationCommitmentScheme" = 
 object 
+        inherit randomization_commitment_scheme
+        method [@static] get_instance : cyclic_group -> int -> permutation_commitment_scheme = "getInstance"
+        method to_string : string = "toString"
+end
 
+class%java proof_system "ch.bfh.unicrypt.crypto.proofsystem.interfaces.ProofSystem" = 
+object 
+      method generate : element -> element -> element = "generate"
+      method verify : element -> element -> bool = "verify"
+      method to_string : string = "toString"
 end 
+
+class%java permutation_commitment_proof_system "ch.bfh.unicrypt.crypto.proofsystem.classes.PermutationCommitmentProofSystem" = 
+object 
+        inherit proof_system
+        method [@static] get_instance : cyclic_group -> int -> permutation_commitment_proof_system = "getInstance"
+        method to_string : string = "toString"
+end
+
+class%java reencryption_shuffle_proof_system "ch.bfh.unicrypt.crypto.proofsystem.classes.ReEncryptionShuffleProofSystem" = 
+object
+        inherit proof_system 
+        method [@static] get_instance : int -> elgamal_encryption_scheme -> element -> reencryption_shuffle_proof_system = "getInstance"
+        method to_string : string = "toString"
+end
+
 
 (* Write small functions to construct these objects, so that subtyping is explicit and don't expose class binding*)
 (* construct big integer from string *)
@@ -232,7 +273,9 @@ let decrypt_message grp gen privatekey encmsg =
    Elgamal_encryption_scheme.decrypt_element elgamal privatekey encmsg  
 
 
-(** val generatePermutation : group -> nat -> 'a1 permutation. This function is taken from extracted code **)
+(** val generatePermutation : group -> nat -> 'a1 permutation. This function is taken from extracted code. 
+    The only difference is here it Java data structure while it's function in OCaml so We need to find a way
+    convert this back and forth  **)
 let generatePermutation grp gen publickey n =
   let elgamal = elgamal_encryption_scheme_from_generator gen in
   let mix = Reencryption_mixer.get_instance elgamal publickey n in
@@ -241,17 +284,94 @@ let generatePermutation grp gen publickey n =
 
 
 (** val generateS : group -> nat -> s **)
-
 let generateS grp gen publickey n = 
-  failwith "implement it" 
- 
+  let perm_com_scheme = Permutation_commitment_scheme.get_instance grp n in 
+  let scm = Randomization_commitment_scheme.get_randomization_space  perm_com_scheme in
+  Set.get_random_element scm
+
+
+(** val generatePermutationCommitment :
+    group -> nat -> 'a1 permutation -> s -> commitment. Remember permutation here is function but Java returns 
+    list so we need a intermediate functions which takes Java Object and turns it into a function **)
+let generatePermutationCommitment grp gen publickey n perm s = 
+  let perm_com_scheme = Permutation_commitment_scheme.get_instance grp n in
+  Randomization_commitment_scheme.commit perm_com_scheme perm s
+
+
+(** val zkpPermutationCommitment :
+    group -> nat -> 'a1 permutation -> commitment -> s -> zKP **)
+
+let zkpPermutationCommitment grp gen publickey n perm comm s = 
+  let pcps = Permutation_commitment_proof_system.get_instance grp n in
+  let offline_private_input = Pair.construct_pair perm s in
+  let offline_public_input  = comm in 
+  Proof_system.generate pcps offline_private_input offline_public_input
+  
+(* Verify the zkp permutation *) 
+let zkpPermutationVerification grp gen publickey n offline_proof offline_public_input = 
+  let pcps = Permutation_commitment_proof_system.get_instance grp n in
+  Proof_system.verify pcps offline_proof offline_public_input
+
+
+(** val homomorphic_addition :
+    group -> ciphertext -> ciphertext -> ciphertext **)
+
+let homomorphic_addition gen grp publickey cone ctwo = cone (* leave it for the moment *)
+
+
+(** val generateR : group -> nat -> r **)
+
+let generateR grp gen publickey n = 
+  let elgamal = elgamal_encryption_scheme_from_generator gen in
+  let mix = Reencryption_mixer.get_instance elgamal publickey n in
+  let r = Mixer.generate_randomizations mix in
+  r
+
+
+(** val shuffle :
+    group -> nat -> ('a1 -> ciphertext) -> 'a1 permutation -> r -> 'a1 ->
+    ciphertext **)
+(* Ballot is a function while it's Tuple in Java. map ballot cand_all and it would give you 
+   list of ciphertext. Convert it into Java array and pass that java array to unicrypt function 
+   which takes java array of ciphertext and prouduces Tuple. 
+   The java function would return tuple which needs to converted back in function form. *)
+(* not tested yet *)
+let shuffle grp gen publickey n ballot perm r =
+  let elgamal = elgamal_encryption_scheme_from_generator gen in
+  let mix = Reencryption_mixer.get_instance elgamal publickey n in
+  let shuffled_ciphertext = Mixer.shuffle mix ballot perm r in
+  shuffled_ciphertext
+
+
+
+(** val shuffle_zkp :
+    group -> nat -> ('a1 -> ciphertext) -> ('a1 -> ciphertext) -> 'a1
+    permutation -> commitment -> s -> r -> zKP **)
+(* Not tested yet! *)
+let shuffle_zkp grp gen publickey n ciphertext shuffled_ciphertext pi cpi s r = 
+   let elgamal = elgamal_encryption_scheme_from_generator gen in 
+   let reencryption_pf_system = Reencryption_shuffle_proof_system.get_instance n elgamal publickey in 
+   let online_private_input = Triple.get_instance pi s r in
+   let online_public_input = Triple.get_instance cpi ciphertext shuffled_ciphertext in 
+   let online_proof = Proof_system.generate reencryption_pf_system online_private_input online_public_input in
+   online_proof
+
+
+(* verify shuffle proof *)
+let shuffle_zkp_verification grp gen publickey n online_proof online_public_input = 
+  let elgamal = elgamal_encryption_scheme_from_generator gen in
+  let reencryption_pf_system = Reencryption_shuffle_proof_system.get_instance n elgamal publickey in
+  Proof_system.verify reencryption_pf_system online_proof online_public_input
+
+
+
+
 
 (*
 let construct_encryption_zero_knowledge_proof grp gen publickey privatekey encmsg = 
    let elgamal = elgamal_encryption_scheme_from_generator gen in
    let dec_msg = decrypt_message grp gen privatekey encmsg in 
-   let enc_last = Pair.get_second encmsg in 
-   Pair.get_second enc_last *)
+   dec_msg *)
 
 let () = 
    let safep = safe_prime (big_int_from_string  "170141183460469231731687303715884114527") in
@@ -263,6 +383,13 @@ let () =
    let encm = encrypt_message grp gen publickey (big_int_from_string "1") (*generate_element_of_group grp (big_int_from_string  "5")*) in
    let decm = decrypt_message grp gen privatekey encm in
    let perm = generatePermutation grp gen publickey 4 in
+   let rands = generateS grp gen publickey 4 in 
+   let pcommit = generatePermutationCommitment grp gen publickey 4 perm rands in
+   let perm_zkp = zkpPermutationCommitment grp gen publickey 4 perm pcommit rands in
+   (* if you have constructed this honestly then it should verify *)
+   let b = zkpPermutationVerification grp gen publickey 4 perm_zkp pcommit in
+   (* generate randomness r*)
+   let r = generateR grp gen publickey 4 in
    print_endline (Prime.to_string safep);
    print_endline (Gstar_mod_safe_prime.to_string grp);
    print_endline (Gstar_mod_element.to_string gen);
@@ -271,8 +398,10 @@ let () =
    print_endline (Zmod_element.to_string privatekey);
    print_endline (Element.to_string encm);
    print_endline (Element.to_string decm);
-   print_endline (Element.to_string perm)
-
-
-
-
+   print_endline (Element.to_string perm);
+   print_endline (Element.to_string rands);
+   print_endline (Element.to_string pcommit);
+   print_endline (Element.to_string perm_zkp);
+   print_endline (if b then "true" else "false"); (* it checks :) *)
+   print_endline (Tuple.to_string r);
+   print_endline (Reencryption_shuffle_proof_system.to_string (Reencryption_shuffle_proof_system.get_instance 4 elgamal publickey))
